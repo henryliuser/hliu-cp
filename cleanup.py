fd493a03e5c8cb1ed81cc1c29ff32bee2d661dd4
Henry Liu <henryliuser@gmail.com>
2021-06-22 23:57:23 -0700

    cleaned up

--- cleanup.py
+++ cleanup.py
@@ -3,7 +3,8 @@ from pathlib import Path
 dir = "/".join(sys.argv[0].split('/')[:-1])
 p = Path(dir)
 
-safe = {".py", ".cpp", ".txt"}
+safeExt = {".py", ".cpp", ".txt"}
+safeName = {".git", ".DS_Store"}
 
 def dfs(pt):
     for x in pt.iterdir():
@@ -11,9 +12,10 @@ def dfs(pt):
         if x.is_dir() and s[-4:] != ".git":
             dfs(x)
             continue
-        name, ext = os.path.splitext(s)
-        if ext not in safe:
-            print(f"rm: {s}")
-            os.remove(s)
+        name, ext = os.path.splitext(os.path.basename(s))
+        if name in safeName or ext in safeExt: continue
+        print(f"rm: {s}")
+        try: os.remove(s)
+        except: print("Failed ^^\n")
 
 dfs(p)
