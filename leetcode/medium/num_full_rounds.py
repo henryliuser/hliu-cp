14e15be20fe33b1f5f1e62aa083b9e4bd856249f
Henry Liu <henryliuser@gmail.com>
2021-06-23 11:03:29 -0700

    hacked contest #2, lost ac rip

--- leetcode/medium/num_full_rounds.py
+++ leetcode/medium/num_full_rounds.py
@@ -1,37 +1,12 @@
 class Solution:
     def numberOfRounds(self, startTime: str, finishTime: str) -> int:
-        def getHM(s):
-            return int(s[0:2]), int(s[3:5])
+        def toM(s):
+            H, M = map(int, s.split(":"))
+            return H*60 + M
 
-        ans = 0
-        lol = range(0, 61, 15)
-        h, m = getHM(startTime)
-        eh, em = getHM(finishTime)
-        if (m % 15 != 0):
-            for l in lol:
-                if (l >= m):
-                    m = l
-                    if m == 60:
-                        m = 0
-                        h = (h+1) % 24
-                    break
-        if (em % 15 != 0):
-            for l in reversed(lol):
-                if (l <= em):
-                    em = l
-                    break
-
-        while h != eh or m != em:
-            for l in lol:
-                if l > m:
-                    m = l
-                    ans += 1
-                    if m == 60:
-                        h = (h+1) % 24
-                        m = 0
-                    break
-
-        return ans
-
-
-            
+        start, end = map(toM, (startTime, finishTime))
+        if 0 <= end - start < 15: return 0
+        start = ceil(start/15) * 15
+        end = end//15 * 15
+        if end < start: end += 1440
+        return (end-start) // 15
