b0117adc19981ac9ccb712391614c14b40172207
Henry Liu <henryliuser@gmail.com>
2021-06-23 03:02:51 -0700

    additional refactor

--- leetcode/medium/replace_substr_balance.py
+++ leetcode/medium/replace_substr_balance.py
@@ -5,23 +5,16 @@ class Solution:
         ans = float('inf')
         count = defaultdict(int)
         for ch in s: count[ch] += 1
-        target = {k: N//4 - count[k] for k in letters}
-        target = {k:v for k,v in target.items() if v < 0}
+        target = {k: count[k] - N//4 for k in letters}
+        target = {k:v for k,v in target.items() if v > 0}
         if len(target) == 0: return 0
         curr = {k:0 for k in letters}
         lo, hi = 0, 0
         while hi < N:
             curr[s[hi]] += 1
-            while all(curr[k] >= -v for k,v in target.items()):
+            while all(curr[k] >= target[k] for k in target):
                 ans = min(ans, hi-lo + 1)
                 curr[s[lo]] -= 1
                 lo += 1
             hi += 1
         return ans
-
-
-
-
-
-
-        
