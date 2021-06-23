ed792b26f3a599f99bd0b12a6a529b8c20d672e7
Henry Liu <henryliuser@gmail.com>
2021-06-23 02:59:35 -0700

    refactor

--- leetcode/medium/replace_substr_balance.py
+++ leetcode/medium/replace_substr_balance.py
@@ -3,17 +3,16 @@ class Solution:
         N = len(s)
         letters = ('Q', 'W', 'E', 'R')
         ans = float('inf')
-        c = defaultdict(int)
-        for ch in s: c[ch] += 1
-        target = {k: N//4 - c[k] for k in letters}
-        if sum(v for k,v in target.items() if v > 0) == 0:
-            return 0
+        count = defaultdict(int)
+        for ch in s: count[ch] += 1
+        target = {k: N//4 - count[k] for k in letters}
+        target = {k:v for k,v in target.items() if v < 0}
+        if len(target) == 0: return 0
         curr = {k:0 for k in letters}
         lo, hi = 0, 0
         while hi < N:
-            ch = s[hi]
-            curr[ch] += 1
-            while all(v >= -target[k] for k,v in curr.items() if target[k] < 0):
+            curr[s[hi]] += 1
+            while all(curr[k] >= -v for k,v in target.items()):
                 ans = min(ans, hi-lo + 1)
                 curr[s[lo]] -= 1
                 lo += 1
