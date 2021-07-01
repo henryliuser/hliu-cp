9e44b493839e4e0b613848c4bca58dbc1492fb7f
Henry Liu <henryliuser@gmail.com>
2021-06-30 18:58:18 -0700

    refactor regex matching

--- leetcode/hard/regex_matching.py
+++ leetcode/hard/regex_matching.py
@@ -1,3 +1,34 @@
+# O(SP) time, O(P) memory
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        S, P = len(s), len(p)
+        dp = [[False] * (P+1) for _ in range(2)]
+
+        dp[0][0] = True  # '' pat matches '' str
+        for c in range(P):  # matching p against ''
+            if c > 0:  # only true if x* matches 0
+                dp[0][c+1] = p[c] == '*' and dp[0][c-1]
+
+        for r in range(S):
+            t = (r+1) % 2
+            nt = not t
+            for c in range(P):
+                if p[c] == '*':
+                    m0 = dp[t][c-1]  # match 0 chars
+                    last = p[c-1]
+                    if last == '.' or last == s[r]:  # match >= 1 chars
+                        dp[t][c+1] = dp[nt][c+1]
+                    if m0:
+                        dp[t][c+1] = True
+                elif p[c] == '.' or s[r] == p[c]:
+                    dp[t][c+1] = dp[nt][c]
+
+            for x in range(len(dp[nt])):  # have to reset
+                dp[nt][x] = False
+
+        return dp[S%2][P]
+
+# O(SP) time, O(SP) memory
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         S, P = len(s), len(p)
@@ -19,7 +50,5 @@ class Solution:
                         dp[r+1][c+1] = True
                 elif p[c] == '.' or s[r] == p[c]:
                     dp[r+1][c+1] = dp[r][c]
-
-        for d in dp: print(list(map(int, d)))
 
         return dp[S][P]
