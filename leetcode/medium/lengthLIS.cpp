ea8374f1baf03f46ee756192f20e1ecd9e348afb
Henry Liu <henryliuser@gmail.com>
2021-06-30 20:02:59 -0700

    binary search psycho solution tails array

--- leetcode/medium/lengthLIS.cpp
+++ leetcode/medium/lengthLIS.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int ans = 1;
+        int ans = 0;
         int N = nums.size();
-        vector<int> dp(N, 1);
-        for (int z = 1; z < N; z++) {
-            int c = nums[z], mx = 0;
-            for (int a = 0; a < z; a++)
-                if (nums[a] < c) mx = max(mx, dp[a]);
-            dp[z] = mx + 1;
-            ans = max(ans, dp[z]);
+        vector<int> dp(N);
+        for (int x : nums) {
+            int lo = 0, hi = ans;
+            while (lo < hi) {
+                int mid = lo + (hi-lo) / 2;
+                if (dp[mid] < x) lo = mid+1;
+                else hi = mid;
+            }
+            dp[lo] = x;
+            if (lo == ans) ans++;
         }
         return ans;
     }
+
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int ans = 1;
+    //     int N = nums.size();
+    //     vector<int> dp(N, 1);
+    //     for (int i = 1; i < N; ++i) {
+    //         int c = nums[i], mx = 0;
+    //         for (int j = 0; j < i; ++j) {
+    //             if (nums[j] < c)
+    //                 mx = max(mx, dp[j]);
+    //         }
+    //         dp[i] = mx + 1;
+    //         ans = max(ans, dp[i]);
+    //     }
+    //     // for (auto d : dp) printf("%d ", d);
+    //     return ans;
+    // }
 };
