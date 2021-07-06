3be3c44dd6143ebc83617b421c89d63a99f8b9b2
Henry Liu <henryliuser@gmail.com>
2021-07-06 02:52:59 -0700

    yes

--- leetcode/medium/videoStitching.cpp
+++ leetcode/medium/videoStitching.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // O(N) greedy
     int videoStitching(vector<vector<int>>& clips, int time) {
         int N = clips.size();
         vector<int> lengths(time+1);
@@ -20,5 +21,25 @@ public:
             right = max(right, lengths[z]);
         }
         return ans;
+    }
+
+    // O(100N) DP
+    int videoStitching(vector<vector<int>>& clips, int time) {
+        int N = clips.size();
+        sort(clips.begin(), clips.end(), [](auto& a, auto& b) {
+            return a[0] < b[0];
+        });
+        vector<int> dp(time+1, N+1);
+        dp[0] = 0;
+        for (int z = 0; z < N; ++z) {
+            int s = clips[z][0];
+            int e = clips[z][1];
+            if (s < 0 || s > time || e < 0) continue;
+            for (int a = s; a <= min(e, time); ++a)
+                dp[a] = min(dp[a], dp[s]+1);
+        }
+        if (dp[time] > N) return -1;
+        return dp[time];
+
     }
 };
