707df1cead559f004eff0edfe736427e58a21dc4
Henry Liu <henryliuser@gmail.com>
2021-08-11 21:42:24 -0700

    refactor

--- leetcode/hard/countDifferentPalindromicSubsequences.cpp
+++ leetcode/hard/countDifferentPalindromicSubsequences.cpp
@@ -4,12 +4,10 @@ public:
     long long MOD = 1e9 + 7;
     vector<vector<long long>> dp;
     long dfs(int i, int j, string& s) {
-        printf("%d %d\n", i, j);
         if (i >  j) return 0;
         if (i == j) return 1;
         if (dp[i][j] != -1) return dp[i][j];
         long mid = dfs(i+1, j-1, s);
-        // printf("mid[%d][%d]: %d\n", i,j, mid);
         char a = s[i], b = s[j];
         if (a != b) {
             dp[i][j] = dfs(i, j-1, s) + dfs(i+1, j, s) - mid;
@@ -24,12 +22,9 @@ public:
                 if (s[z] == a) {
                     R = z; break;
                 }
-            printf("%d %d | %d %d\n", i,j, L, R);
             if (L == -1) dp[i][j] = 2*mid + 2;
             else if (L == R) dp[i][j] = 2*mid + 1;
             else dp[i][j] = 2*mid - dfs(L+1, R-1, s);
-            // printf("dp[%d][%d] = %lld\n", i, j, dp[i][j]);
-
         }
         dp[i][j] = (dp[i][j] + MOD) % MOD;
         return dp[i][j];
@@ -38,6 +33,6 @@ public:
     int countPalindromicSubsequences(string s) {
         N = s.size();
         dp.assign(N+1, vector<long long>(N+1, -1));
-        return dfs(0, N-1, s) % MOD; 
+        return dfs(0, N-1, s) % MOD;
     }
 };
--- other/getRandom.py
+++ other/getRandom.py
@@ -1,18 +1,27 @@
+from random import randint
 # O(calls)
-def get_random(L):
+def get_random1(L):
+    C = list(L)
     while True:
         size = len(L)
         while size > 0:
             size -= 1
             i = randint(0, size)
-            L[i], L[size] = L[size], L[i]
-            yield L[size]
+            C[i], C[size] = C[size], C[i]
+            yield C[size]
 
 # O(2 * calls + 10) lmao
-def get_random(L):
+def get_randomN(L):
     while True:
         copy = list(L)
         while L:
             i = randint(0, len(L)-1)
             L[i], L[-1] = L[-1], L[i]
             yield L.pop()
+
+if __name__ == '__main__':
+    a = [randint(0,1000) for _ in range(5)]
+    g = get_random1(a)
+    print(a)
+    for _ in range(100):
+        print(next(g))
