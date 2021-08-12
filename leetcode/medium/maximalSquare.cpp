8d972c7e638d1494baae00b42c881ca69864fe95
Henry Liu <henryliuser@gmail.com>
2021-08-12 00:39:27 -0700

    maximal square O(N) memory

--- leetcode/medium/maximalSquare.cpp
+++ leetcode/medium/maximalSquare.cpp
@@ -4,16 +4,17 @@ public:
         int ans = 0;
         int M = A.size();
         int N = A[0].size();
-        vector<vector<int>> dp(M+1, vector<int>(N+1));
+        vector<vector<int>> dp(2, vector<int>(N+1));
         for (int r = 1; r <= M; ++r) {
+            int t = r % 2;
             for (int c = 1; c <= N; ++c) {
-                if (A[r-1][c-1] == '0') continue;
-                int W = dp[r][c-1];
-                int NW = dp[r-1][c-1];
-                int N = dp[r-1][c];
-                if (W == NW && W == N)  dp[r][c] = W+1;
-                else dp[r][c] = min({W, NW, N}) + 1;
-                ans = max(ans, dp[r][c]);
+                if (A[r-1][c-1] == '0') { dp[t][c] = 0; continue; }
+                int W = dp[t][c-1];
+                int NW = dp[!t][c-1];
+                int N = dp[!t][c];
+                if (W == NW && W == N)  dp[t][c] = W+1;
+                else dp[t][c] = min({W, NW, N}) + 1;
+                ans = max(ans, dp[t][c]);
             }
         }
         return ans * ans;
