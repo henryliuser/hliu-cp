c983a49969ee20a2b0394a0dddffa0557b0ff7b8
Henry Liu <henryliuser@gmail.com>
2021-08-15 12:20:32 -0700

    counter

--- leetcode/medium/combination_sum_II.py
+++ leetcode/medium/combination_sum_II.py
@@ -1,8 +1,8 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
-        C = Counter(candidates)
-        nums = sorted(C)
+        count = Counter(candidates)
+        nums = sorted(count)
         N = len(nums)
         def dfs(i, curr, path):
             if curr == target:
@@ -10,12 +10,12 @@ class Solution:
             for x in range(i, N):
                 q = nums[x]
                 if curr+q > target: break
-                if C[q] <= 0: continue
-                C[q] -= 1
+                if count[q] <= 0: continue
+                count[q] -= 1
                 path.append(q)
                 dfs(x, curr+q, path)
                 path.pop()
-                C[q] += 1
+                count[q] += 1
         dfs(0, 0, [])
         return ans
 
