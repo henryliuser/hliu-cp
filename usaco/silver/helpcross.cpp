d276cec765e8769c1aec35c3b929e068b231b207
Henry Liu <henryliuser@gmail.com>
2021-08-28 15:02:20 -0400

    helpcross

--- usaco/silver/helpcross.cpp
+++ usaco/silver/helpcross.cpp
@@ -1,42 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-// O(N log N) greedy match 2 pointers? -- doesn't work
+
 int main() {
     freopen("helpcross.in", "r", stdin);
     freopen("helpcross.out", "w", stdout);
-    int C, N; cin >> C >> N;
-    vector<int> T(C);
-    vector<pair<int, int>> cows(N);
-    for (int z = 0; z < C; ++z)
-        cin >> T[z];
-    for (int z = 0; z < N; ++z)
-        cin >> cows[z].first >> cows[z].second;
-
-    sort(T.begin(), T.end());
-    sort(cows.begin(), cows.end(), [](auto& a, auto& b) {
-        if (a.second < b.second) return true;
-        // if (a.first > b.first) return true;
-        return a.first < b.first;
-    });
-
-    // for (auto d : cows) cout << d.first << " "; cout << endl;
-    // for (auto d : cows) cout << d.second << " "; cout << endl;
-
     int ans = 0;
-    int i = 0, j = 0;  // i: chickens,  j: cows
-    while (i < C && j < N) {
-        // printf("%d %d: ", i, j);
-        int t = T[i];
-        auto& c = cows[j];
-        int a = c.first, b = c.second;
-        if (a <= t && t <= b) {
-            ++ans, ++i, ++j;
-        }
-        if (a > t) ++i;
-        if (b < t) ++j;
-        // printf("%d\n", ans);
+    int C, N; cin >> C >> N;
+    multiset<int> chickens;
+    vector<pair<int, int>> cows(N);
+    for (int z = 0; z < C; ++z) {
+        int x; cin >> x;
+        chickens.insert(x);
     }
-
+    for (int z = 0; z < N; ++z) {
+        int end, start; cin >> end >> start;
+        cows[z] = {start, end};  // Reversed order, for sort
+    }
+    sort(cows.begin(), cows.end());
+    for (int z = 0; z < N; ++z) {
+        auto [end, start] = cows[z];
+        auto c = chickens.lower_bound(start);  // find earliest >= start
+        if (c == chickens.end()) continue;
+        if (*c <= end) {  // if valid, take
+            ++ans;
+            chickens.erase(c);
+        }
+    }
     cout << ans << endl;
-
 }
