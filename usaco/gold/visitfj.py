b6aa6f320cadcd32a6bce9a4eabcb4213c0d8795
Henry Liu <henryliuser@gmail.com>
2021-08-31 03:03:23 -0400

    refactor

--- usaco/gold/visitfj.py
+++ usaco/gold/visitfj.py
@@ -18,7 +18,7 @@ def build_graph():
     return graph
 
 def dijkstra(graph):
-    pq = [(0,0,0,-1)]  # (p, r, c, v)
+    pq = [(0,0,0,-1)]  # (priority, row, column, cycle)
     seen = set()
     dist = defaultdict(lambda: float('inf'))
     dist[(0,0,-1)] = 0
@@ -42,80 +42,4 @@ if __name__ == '__main__':
     N, T = intput()
     grid = [list(intput()) for _ in range(N)]
     graph = build_graph()
-    # for g in graph:
-    #     print(f"{g}: {graph[g]}")
     print(dijkstra(graph))
-
-
-# import sys
-# dirs = ((1,0), (0,1), (-1,0), (0,-1))
-#
-# def dfs(r, c, curr, v):
-#     print(r,c,curr,v)
-#     for pd in dp:
-#         print(pd)
-#     if (r,c) == (N-1, N-1):
-#         return curr
-#     if dp[r][c][v] != -1:
-#         return dp[r][c][v]
-#     res = float('inf')
-#     for d in dirs:
-#         x, y = r+d[0], c+d[1]
-#         if x < 0 or x >= N or y < 0 or y >= N:
-#             continue
-#         next = (v+1) % 3
-#         val = curr + T
-#         if next == 2: val += grid[x][y]
-#         res = min(res, dfs(x, y, val, next))
-#     if (r,c) == (3,2): print("HERE")
-#     dp[r][c][v] = res
-#     return res
-#
-# if __name__ == '__main__':
-#     ans = float('inf')
-#     # sys.stdin  = open('visitfj.in',  'r')
-#     # sys.stdout = open('visitfj.out', 'w')
-#     intput = lambda: map(int, input().split())
-#     N, T = intput()
-#     grid = [list(intput()) for _ in range(N)]
-#     dp = [[[-1]*3 for _ in range(N)] for _ in range(N)]
-#     print( dfs(0,0,0,-1) )
-
-
-
-# import sys
-# from heapq import heappush, heappop
-# dirs = ((0,1), (1,0), (-1,0), (0,-1))
-#
-# def bfs():
-#     seen = [[[False] * 3 for _ in range(N)] for _ in range(N)]
-#     pq = [(0,0,0,-1)]  # (curr, r,c, v)
-#     ans = float('inf')
-#     while pq:
-#         curr, r, c, v = heappop(pq)
-#         if curr > limit: continue
-#         if seen[r][c][v]: continue
-#         seen[r][c][v] = True
-#         for d in dirs:
-#             x, y = r+d[0], c+d[1]
-#             if x < 0 or x >= N or y < 0 or y >= N:
-#                 continue
-#             val = curr + T
-#             if v == 2:
-#                 val += grid[x][y]
-#             if val > limit:
-#                 continue
-#             if (x,y) == (N-1, N-1):
-#                 ans = min(ans, val)
-#             heappush(pq, (val, x, y, (v+1)%3))
-#     return ans < limit
-#
-#
-# if __name__ == '__main__':
-#     # sys.stdin  = open('visitfj.in',  'r')
-#     # sys.stdout = open('visitfj.out', 'w')
-#     intput = lambda: map(int, input().split())
-#     N, T = intput()
-#     grid = [list(intput()) for _ in range(N)]
-#     bfs()
-#     print(ans)
