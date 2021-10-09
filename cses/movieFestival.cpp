3a7fd48ce8fbd6217ec8d03e9c2a8e25a0d7a6d2
Henry Liu <henryliuser@gmail.com>
2021-10-09 09:33:38 -0400

    movie festival

--- cses/movieFestival.cpp
+++ cses/movieFestival.cpp
@@ -4,7 +4,7 @@ using pii = pair<int, int>;
 #define f first
 #define s second
 
-int dp[20001];
+int dp[200001];
 int main() {
     int n; cin >> n;
     vector<pii> movies(n);
@@ -12,10 +12,14 @@ int main() {
         cin >> movies[i].s >> movies[i].f;
 
     sort(begin(movies), end(movies));
+    vector<int> ends(n);
+    for (int i = 0; i < n; ++i)
+        ends[i] = movies[i].f;
     for (int i = 0; i < n; ++i) {
         dp[i] = 1;
         pii& m = movies[i];
-        int j = movies.lower_bound({m.s, -1}) - movies.begin();
+        auto it = lower_bound(ends.begin(), ends.end(), m.s);
+        int j = it - ends.begin();
         if (movies[j].f > m.s) --j;
         if (j >= 0) dp[i] += dp[j];
         dp[i] = max(dp[i], dp[i-1]);
