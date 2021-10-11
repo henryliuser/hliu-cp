8e1e603c2214e375140b58ce2fbd194da7edf01f
Henry Liu <henryliuser@gmail.com>
2021-10-11 16:05:40 -0400

    gold merge sort tree

--- usaco/gold/unitedCowsFarmerJohn.cpp
+++ usaco/gold/unitedCowsFarmerJohn.cpp
@@ -2,30 +2,57 @@
 using namespace std;
 using ll = long long;
 
+struct MergeSortTree {
+    unsigned long N;
+    vector<vector<int>> T;  // tree representation
+    vector<int> &A;  // array reference
+    void build(int v, int tl, int tr) {
+        if (tl == tr) T[v] = {A[tl]};
+        else {
+            int mid = (tl + tr) / 2;
+            build(v*2, tl, mid);
+            build(v*2+1, mid+1, tr);
+            T[v].resize(tr-tl+1);
+            auto &l = T[v*2], &r = T[v*2+1];
+            merge(begin(l), end(l), begin(r), end(r), begin(T[v]));
+        }
+    }
+    int query(int al, int ar) { return query(al, ar, ar, 1, 0, N-1); }
+    int query(int al, int ar, int R, int v, int tl, int tr) {
+        if (al > ar) return 0;
+        if (al == tl && ar == tr)
+            return end(T[v]) - upper_bound(begin(T[v]), end(T[v]), R);
+        int mid = (tl + tr) / 2;
+        int ql = query(al, min(ar, mid), R, v*2, tl, mid);
+        int qr = query(max(al, mid+1), ar, R, v*2+1, mid+1, tr);
+        return ql + qr;
+    }
+    MergeSortTree(unsigned long n, vector<int>& a)
+        : N{n}, A{a}, T{4*n, vector<int>()} { build(1,0,N-1); }
+};
+
 int main() {
     ll ans = 0;
-    int N; cin >> N;
+    unsigned long N; cin >> N;
     vector<int> cows(N);
     for (int i = 0; i < N; ++i)
         cin >> cows[i];
     unordered_map<int, int> next;
-    next[cows[N-1]] = N-1;
-    vector<int> suf(N+1);
-    for (int i = N-2; i >= 0; --i) {
+    vector<int> neq(N);
+    for (int i = N-1; i >= 0; --i) {
         int x = cows[i];
-        suf[i] = suf[i+1];
-        // printf("%d %d\n", i, x);
-        if (next.count(x)) {
-            ans += next[x] - i - 1 - (suf[i] - suf[next[x]]);
-            next[x] = i;
-            ++suf[i];
-            continue;
-        }
-        ans += next.size();
+        neq[i] = INT_MAX;
+        if (next.count(x))
+            neq[i] = next[x];
         next[x] = i;
-        // printf("%d\n", ans);
+    }
+    MergeSortTree seg{N, neq};
+
+    for (int i = 0; i < N; ++i) {
+        int x = cows[i];
+        int j = min(neq[i]-1, (int)N-1);
+        ans += seg.query(i+1, j);
     }
     cout << ans << endl;
-
 
 }
