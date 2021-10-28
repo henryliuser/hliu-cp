c94dbe5e765349480e31f847807a3bc66adfada7
Henry Liu <henryliuser@gmail.com>
2021-10-28 14:05:53 -0400

    dsu binary search

--- other/testtt.py
+++ other/testtt.py
@@ -1,21 +1,20 @@
-# names = ['asd', 'bcd', 'qwe', 'asd']
-# numbers = [123, 554, 231, 444]
-#
-# d = {name:num for name, num in reversed(list(zip(names, numbers)))}
-#
-# A = [[1,2,3,] , [12,3,] [...]]
-# for i in range(len(A)):
-#     for j in range(len(A[i])):
-#         x = A[i][j]
-#
-# # d = {}
-# # for name, num in reversed(list(zip(names, numbers))):
-# #     d[name] = num
-# print(d)
+def fp(A, pl, pr):
+    def dfs(L, R, c):
+        if c == len(A):
+            print(f"L: {L}")
+            print(f"R: {R}")
+            # rl, rr = pl(L), pr(r)
+            # if rl and rr:
+            #     return rl, rr
+            return None
+        L.append(A[c])
+        rl = dfs(L, R, c+1)
+        if rl: return rl
+        R.append(L.pop())
+        rr = dfs(L, R, c+1)
+        if rr: return rr
+        R.pop()
+    dfs([], [], 0)
 
-a = "asd qyweuhjk yatguhdjilka uayshifojqdkw"
-print(a.split(' '))
-
-line = "Hamilton: yughasjda"
-
-print(line.split(': '))
+A = [0,1,2,3,4,5,6,7,8,9]
+fp(A, lambda x:x, lambda x:x)
---
+++ usaco/silver/wormsort.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+using tup = tuple<int, int, int>;
+
+int N, M;
+int a, b, w;
+vector<int> A;
+vector<tup> edges;
+
+struct DSU {
+    int count;
+    vector<int> size, parent;
+    bool unite(int a, int b) {
+        a = find(a), b = find(b);
+        if (a == b) return false;
+        if (size[a] < size[b]) swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+        --count;
+        return true;
+    }
+    int find(int a) {
+        if (parent[a] == -1) return a;
+        return parent[a] = find(parent[a]);
+    }
+    DSU(int n) : count(n), size(n, 1), parent(n, -1) {}
+};
+
+bool check(int g) {
+    DSU uf(N+1);
+    for (int i = g; i < M; ++i) {
+        tie(w,a,b) = edges[i];
+        uf.unite(a, b);
+    }
+    for (int i = 0; i < N; ++i) {
+        if (A[i] == i+1) continue;
+        if (uf.find(A[i]) != uf.find(i+1))
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    freopen("wormsort.in", "r", stdin);
+    freopen("wormsort.out", "w", stdout);
+    cin >> N >> M;
+    A.resize(N);
+    edges.resize(M);
+    bool sorted = true;
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+        if (i > 0 && A[i] < A[i-1])
+            sorted = false;
+    }
+    if (sorted) {
+        cout << -1 << endl;
+        return 0;
+    }
+    for (int i = 0; i < M; ++i) {
+        cin >> a >> b >> w;
+        edges[i] = {w,a,b};
+    }
+    sort(begin(edges), end(edges));
+    int lo = 0, hi = M;
+    while (lo < hi) {
+        int mid = lo + ceil((hi-lo) / 2.0);
+        if (check(mid)) lo = mid;
+        else hi = mid-1;
+    }
+    cout << get<0>(edges[lo]) << endl;
+
+
+}
