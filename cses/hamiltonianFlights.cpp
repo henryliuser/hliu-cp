d0145373d2a3c6e33368001ae8ff3a18e1d1fe08
Henry Liu <henryliuser@gmail.com>
2021-11-05 06:59:41 -0400

    bitmask DP hamiltonian flights

--- cses/hamiltonianFlights.cpp
+++ cses/hamiltonianFlights.cpp
@@ -1,28 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9 + 7;
+using ll = long long;
 
 int N, M;
-int ans = 0;
-// int dp[1 << 20];
-unordered_set<int> s;
+ll dp[1 << 20][20] = {};  // dp[s][i] = # routes that visit s, end in i
 vector<vector<int>> adj;
-
-void dfs(int u) {
-    s.insert(u);
-    if (s.size() == N) ++ans;
-    for (int v : adj[u])
-        if (!s.count(v))
-            dfs(v);
-    s.erase(u);
-}
 
 int main() {
     cin >> N >> M;
-    adj.resize(N+1);
+    int all = (1 << N) - 1;
+    adj.resize(N--);
     while (M--) {
         int a, b; cin >> a >> b;
-        adj[a].push_back(b);
+        adj[--b].push_back(--a);  // invert the graph
     }
-    dfs(1);
-    cout << ans << endl;
+
+    dp[1][0] = 1;  // 1 route, visits city 0, ending in city 0
+    // ^^^ this base case also ensures we only count routes starting at 1
+    for (int S = 3; S <= all; S += 2) {  // skip all without 1
+        // bool has1 = S & 1;
+        bool hasN = S & (1 << N);
+        bool full = S == all;
+        // if (!has1) continue;  // subset must include city 1
+        if (!full && hasN) continue;  // subset can't have N unless full
+        for (int i = 1; i <= N; ++i) {
+            int mask = S ^ (1 << i);
+            for (int x : adj[i]) {
+                bool hasX = S & (1 << x);
+                if (!hasX) continue;
+                dp[S][i] += dp[mask][x];
+                dp[S][i] %= MOD;
+            }
+        }
+    }
+    cout << dp[all][N] << endl;
 }
