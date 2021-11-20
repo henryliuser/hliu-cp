cbe44a8442f9bb44cf2832f222479ce0403d34fd
Henry Liu <henryliuser@gmail.com>
2021-11-20 18:49:55 -0500

    collecting nums

--- cses/collectingNumbers.cpp
+++ cses/collectingNumbers.cpp
@@ -1,19 +1,50 @@
+// O(n)
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int ans = 1;
-    int n; cin >> n;
-    int st = 1;
-    vector<int> A(n);
-    cin >> A[0];
-    for (int i = 1; i < n; ++i) {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int N; cin >> N;
+    vector<int> A(N);
+    unordered_map<int, int> idx;
+    idx.reserve(N);
+    for (int i = 0; i < N; ++i) {
         cin >> A[i];
-        if (A[i] < A[i-1])
-            ans = max(ans, ++st);
-        else
-            st = 1;
+        idx[A[i]] = i;
     }
+    int last = -1;
+    for (int i = 1; i <= N; ++i) {
+        if (idx[i] < last) ++ans;
+        last = idx[i];
+    }
+    cout << ans << "\n";
 
-    cout << ans << endl;
 }
+
+// O(n log n) bs
+//
+// #include <bits/stdc++.h>
+// using namespace std;
+//
+// int main() {
+//     cin.tie(0);
+//     ios::sync_with_stdio(0);
+//     int ans = 1;
+//     int N; cin >> N;
+//     map<int, int> idx;
+//     for (int i = 0; i < N; ++i) {
+//         int x; cin >> x;
+//         idx.insert({x, i});
+//     }
+//
+//     int last = -1;
+//     for (int i = 1; i <= N; ++i) {
+//         int j = idx[i];
+//         if (j < last) ++ans;
+//         last = j;
+//     }
+//     cout << ans << "\n";
+//
+// }
