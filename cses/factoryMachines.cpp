010a9efa553110d987759645f4c0672c86c77fb7
Henry Liu <henryliuser@gmail.com>
2021-11-21 17:48:58 -0500

    spaces

--- cses/factoryMachines.cpp
+++ cses/factoryMachines.cpp
@@ -6,21 +6,21 @@ ll N, T;
 vector<ll> A;
 
 bool check(ll g) {
-	ll res = 0;
-	for (int i = 0; i < N; ++i)
-		res += g / A[i];
-	return res >= T;
+    ll res = 0;
+    for (int i = 0; i < N; ++i)
+        res += g / A[i];
+    return res >= T;
 }
 
 int main() {
-	cin >> N >> T; A.resize(N);
-	for (ll &x : A) cin >> x;
+    cin >> N >> T; A.resize(N);
+    for (ll &x : A) cin >> x;
 
-	ll lo = 1, hi = LLONG_MAX-100;
-	while (lo < hi) {
-		ll mid = lo + (hi-lo) / 2;
-		if (check(mid)) hi = mid;
-		else lo = mid + 1;
-	}
-	cout << lo << "\n";
+    ll lo = 1, hi = LLONG_MAX-100;
+    while (lo < hi) {
+        ll mid = lo + (hi-lo) / 2;
+        if (check(mid)) hi = mid;
+        else lo = mid + 1;
+    }
+    cout << lo << "\n";
 }
