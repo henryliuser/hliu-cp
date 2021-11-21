81af511c7b6c780af4c805cbe605bd53662d3f9a
Henry Liu <henryliuser@gmail.com>
2021-11-21 17:48:48 -0500

    spaces

--- usaco/silver/cowDanceShow.cpp
+++ usaco/silver/cowDanceShow.cpp
@@ -5,38 +5,38 @@ int N, T;
 vector<int> D;
 
 bool check(int g) {
-	int t = 0;
-	multiset<int> curr;
-	for (int i = 0; i < N; ++i) {
-		curr.insert( D[i] + t );
-		if (curr.size() > g) {
-			auto it = curr.begin();
-			t = *it;
-			while (*it == t) {
-				t = *it;
-				curr.erase(it);
-				it = curr.begin();
-			}
-			t = *it;
-		}
-	}
-	t = *curr.rbegin();
-	return t <= T;
+    int t = 0;
+    multiset<int> curr;
+    for (int i = 0; i < N; ++i) {
+        curr.insert( D[i] + t );
+        if (curr.size() > g) {
+            auto it = curr.begin();
+            t = *it;
+            while (*it == t) {
+                t = *it;
+                curr.erase(it);
+                it = curr.begin();
+            }
+            t = *it;
+        }
+    }
+    t = *curr.rbegin();
+    return t <= T;
 }
 
 int main() {
-	freopen("cowdance.in", "r", stdin);
-	freopen("cowdance.out", "w", stdout);
-	cin.tie(0);
-	ios::sync_with_stdio(0);
-	cin >> N >> T; D.resize(N);
-	for (int &x : D) cin >> x;
+    freopen("cowdance.in", "r", stdin);
+    freopen("cowdance.out", "w", stdout);
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin >> N >> T; D.resize(N);
+    for (int &x : D) cin >> x;
 
-	int lo = 1, hi = N;
-	while (lo < hi) {
-		int mid = lo + (hi-lo) / 2;
-		if (check(mid)) hi = mid;
-		else lo = mid + 1;
-	}
-	cout << lo << "\n";
+    int lo = 1, hi = N;
+    while (lo < hi) {
+        int mid = lo + (hi-lo) / 2;
+        if (check(mid)) hi = mid;
+        else lo = mid + 1;
+    }
+    cout << lo << "\n";
 }
