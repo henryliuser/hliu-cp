2e6ce12e0607c9672858637085ec9cd5587dcd4f
Henry Liu <henryliuser@gmail.com>
2022-01-03 20:49:28 -0800

    refactor

--- codeforces/L0/integer_diversity.py
+++ codeforces/L0/integer_diversity.py
@@ -6,13 +6,10 @@ intput = lambda : map(int, input().split())
 def solve():
     N, = intput()
     A  = list(intput())
-
     cnt = defaultdict(int)
     for x in A:
-        if cnt[x] == 0:
-            cnt[x] += 1
-        else:
-            cnt[-x] += 1
+        if cnt[x] == 0: cnt[x] += 1
+        else: cnt[-x] += 1
     return len(cnt)
 
 if __name__ == '__main__':
