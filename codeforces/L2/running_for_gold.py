59aa4a63304f73a0fbfb1f99c2ea6c49d6dad37c
Henry Liu <henryliuser@gmail.com>
2022-01-14 22:41:34 -0800

    randomized algo

---
+++ codeforces/L2/running_for_gold.py
@@ -0,0 +1,24 @@
+import sys, random
+input = lambda : sys.stdin.readline().strip()
+intput = lambda : map(int, input().split())
+
+def solve():
+    N, = intput()
+    rank = [list(intput()) for _ in range(N)]
+    def beats(i, j):  # i beats itself
+        res = 0
+        for k in range(5):
+            res += (rank[i][k] <= rank[j][k])
+        return res >= 3
+
+    ord = list(range(N))
+    random.shuffle(ord)
+    for i in range(N):
+        if all( beats(i, ord[j]) for j in range(N) ):
+            return i+1
+    return -1
+
+if __name__ == '__main__':
+    T, = intput()
+    for _ in range(T):
+        print( solve() )
