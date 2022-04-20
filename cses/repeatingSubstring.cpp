fadeb9b524d32b62d9494a08e6c2e1209f545fd1
Henry Liu <henryliuser@gmail.com>
2022-04-20 02:23:49 -0400

    asd

---
+++ cses/repeatingSubstring.cpp
@@ -0,0 +1,100 @@
+// https://cses.fi/problemset/task/2420
+// binary search rabin karp
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+struct PolyHash {
+    // -------- Static variables --------
+    static const int mod = (int)1e9+123; // prime mod of polynomial hashing
+    static std::vector<int> pow1;        // powers of base modulo mod
+    static std::vector<ull> pow2;        // powers of base modulo 2^64
+    static int base;                     // base (point of hashing)
+    static int mxPow;
+
+    // --------- Static functons --------
+    static inline int diff(int a, int b) {
+        // Diff between `a` and `b` modulo mod (0 <= a < mod, 0 <= b < mod)
+        return (a -= b) < 0 ? a + mod : a;
+    }
+
+    // Generate random base in (before, after) open interval:
+    static int gen_base(const int before, const int after) {
+        auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+        std::mt19937 mt_rand(seed);
+        int base = std::uniform_int_distribution<int>(before+1, after)(mt_rand);
+        return base % 2 == 0 ? base-1 : base;
+    }
+
+    // -------------- Variables of class -------------
+    std::vector<int> pref1; // Hash on prefix modulo mod
+    std::vector<ull> pref2; // Hash on prefix modulo 2^64
+
+    // Cunstructor from string:
+    PolyHash(const std::string& s)
+            : pref1(s.size()+1u, 0)
+            , pref2(s.size()+1u, 0)
+    {
+        mxPow = max(mxPow, (int)s.size());
+        assert(base < mod);
+        const int n = s.size(); // Firstly calculated needed power of base:
+        while ((int)pow1.size() <= n) {
+            pow1.push_back(1LL * pow1.back() * base % mod);
+            pow2.push_back(pow2.back() * base);
+        }
+        for (int i = 0; i < n; ++i) { // Fill arrays with polynomial hashes on prefix
+            assert(base > s[i]);
+            pref1[i+1] = (pref1[i] + 1LL * s[i] * pow1[i]) % mod;
+            pref2[i+1] = pref2[i] + s[i] * pow2[i];
+        }
+    }
+
+    // Polynomial hash of subsequence [pos, pos+len)
+    // If mxPow != 0, value automatically multiply on base in needed power. Finally base ^ mxPow
+    inline std::pair<int, ull> operator()(const int pos, const int len) const {
+        int hash1 = pref1[pos+len] - pref1[pos];
+        ull hash2 = pref2[pos+len] - pref2[pos];
+        if (hash1 < 0) hash1 += mod;
+        if (mxPow != 0) {
+            hash1 = 1LL * hash1 * pow1[mxPow-(pos+len-1)] % mod;
+            hash2 *= pow2[mxPow-(pos+len-1)];
+        }
+        return {hash1, hash2};
+    }
+};
+int PolyHash::mxPow = 0;
+int PolyHash::base((int)1e9+7);
+std::vector<int> PolyHash::pow1{1};
+std::vector<ull> PolyHash::pow2{1};
+// remember to gen_base
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    PolyHash::base = PolyHash::gen_base(256, PolyHash::mod);
+
+    string s; cin >> s;
+    int N = s.size();
+
+    PolyHash h(s);
+    int l = 0, r = N;
+    array<int, 2> ans = {-1,-1};
+    while (l < r) {
+        int i;
+        int m = (l+r+1) >> 1;  // len
+        set<pair<int,ull>> vis;
+        for (i = 0; i <= N-m; ++i) {
+            auto x = h(i, m);
+            if (vis.count(x)) goto ok;
+            vis.insert(x);
+        }
+        r = m-1;
+        continue;
+        ok: l = m;
+        ans = {i,m};
+    }
+    auto [i,m] = ans;
+    cout << (i == -1 ? "-1" : s.substr(i,m)) << '\n';
+}
