56c655865c5d8541ad3234066cd92c0de7eb1b27
Henry Liu <henryliuser@gmail.com>
2022-06-19 11:43:27 -0500

    oops

--- codeforces/zeroPath.cpp
+++ codeforces/zeroPath.cpp
@@ -3,11 +3,11 @@
 #pragma GCC optimize("O3")
 #pragma GCC target("sse4")
 using namespace std;
-#define int uint64_t
+
 static constexpr int MXN = 1001;
 
-int A[MXN][MXN];
-bitset<MXN> dp[MXN][MXN];
+short A[MXN][MXN];
+bitset<1005> dp[MXN][MXN];
 
 string_view solve() {
     int N, M;
@@ -16,6 +16,8 @@ string_view solve() {
         for (int j = 1; j <= M; ++j)
             cin >> A[i][j];
 
+    int K = N+M-1;
+    if (K % 2) return "No";
     for (int i = 0; i <= N; ++i)
         for (int j = 0; j <= M; ++j)
             dp[i][j] = 0;
@@ -25,12 +27,10 @@ string_view solve() {
         for (int j = 1; j <= M; ++j) {
             auto L = dp[i][j-1];
             auto U = dp[i-1][j];
-            if (A[i][j] == 1)
-                dp[i][j] = (L << 1) | (U << 1);
-            else
-                dp[i][j] = (L >> 1) | (U >> 1);
+            if (A[i][j] == -1) dp[i][j] = L | U;
+            else dp[i][j] = (L << 1) | (U << 1);
         }
-    return (dp[N][M][0] ? "Yes" : "No");
+    return (dp[N][M][K/2] ? "Yes" : "No");
 }
 
 signed main() {
